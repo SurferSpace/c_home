02138767226a449e778bb90164fbaf97bac35618
SurferSpace <138975483+SurferSpace@users.noreply.github.com>
2023-07-29 20:40:51 +0300

    New

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿/*Console.WriteLine("Введите трехзначное число: ");
+int tr = int.Parse(Console.ReadLine()!);
+string trstring = tr.ToString();
+if(trstring.Length == 3){
+    Console.WriteLine(trstring[1]);
+}
+else Console.WriteLine("Число не корректно.");*/
+
+/*Console.Write("Введите номер дня недели: ");
+int week = int.Parse(Console.ReadLine()!);
+if (week > 0 && week <8){
+    string result = (week == 6 ||week == 7 ? "Выходной" : "Рабочий день");
+    Console.WriteLine(result);
+}
+else Console.WriteLine("День недели введен не корректно. ");*/
+
+/*Console.WriteLine("Введите трехзначное число: ");
+int tr = int.Parse(Console.ReadLine()!);
+string trstring = tr.ToString();
+Console.WriteLine(trstring[1]);*/
